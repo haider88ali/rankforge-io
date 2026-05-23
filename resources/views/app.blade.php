67cf6e79b4847938b61c9c4d5b3bc6f98e934386
Haider Ali <77407876+haider88ali@users.noreply.github.com>
2026-05-23 22:52:54 +0500

    add changes for fav icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'SeoKitHub') }}</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml" title="{{ config('app.name', 'SeoKitHub') }}">
+        <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+        <meta name="theme-color" content="#4f46e5">
+        <meta name="application-name" content="{{ config('app.name', 'SeoKitHub') }}">
+        <meta name="apple-mobile-web-app-title" content="{{ config('app.name', 'SeoKitHub') }}">
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         @inertiaHead
